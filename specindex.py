2da5807837f0bebc5e9a5df957051c4431e57d65
Mark Richardson <Mark.Richardson.Work@gmail.com>
2021-08-28 20:24:54 -0400

    Added Const L spec index plot, and move spec index error map to new tutorial.

--- specindex.py
+++ specindex.py
@@ -48,7 +48,7 @@ def __stretch__(p,s1,f1):
         return ( (p-f0) + (1.-p)*f1 )/(1-f0)
 
 
-def create_cmap_specindex(minp,maxp,steepp=-0.8,flatp=-0.1,name="CC-specindex-default",modes=['clip','crop'],targets=['mpl','png'],png_dir=".",out=False):
+def create_cmap_specindex(min_p,max_p,steep_p=-0.8,flat_p=-0.1,name="CC-specindex-default",modes=['clip','crop'],targets=['mpl','png'],png_dir=".",out=False):
     """ Makes a new colour map based on Jayanne English's colourmap
         of yellow - plum, where the orange and dark cyan points
         are fixed to the steep and flat components, while the outer
@@ -62,32 +62,32 @@ def create_cmap_specindex(minp,maxp,steepp=-0.8,flatp=-0.1,name="CC-specindex-de
     # flatcolor = '#004961'   # dark cyan blue
     # endcolor = '#390358'    # dark magenta-purple i.e. plum
     #
-    # minp = -1.3
-    # maxp = 0.3
-    # steepp = -0.8
-    # flatp = -0.1
-    #   --> s = (steepp - minp)/(maxp - minp) = 0.3125
-    #   --> f = (flatp - minp)/(maxp - minp) = 0.75
+    # min_p = -1.3
+    # max_p = 0.3
+    # steep_p = -0.8
+    # flat_p = -0.1
+    #   --> s = (steep_p - min_p)/(max_p - min_p) = 0.3125
+    #   --> f = (flat_p - min_p)/(max_p - min_p) = 0.75
     #   --> m = (s+f)/2 = 0.53125
     #
-    # The designed colour map defines the step portion in terms of minp - s,
-    #  the flat portion in terms of f - maxp, and the intermediate s - p.
+    # The designed colour map defines the step portion in terms of min_p - s,
+    #  the flat portion in terms of f - max_p, and the intermediate s - p.
     #  As the user can define all four of these, we need to stretch the
     #  colourmap as necessary to preserve the features designed into the
     #  colourmap. This is established with the __stretch__ command.
 
-    color_width = maxp - minp
+    color_width = max_p - min_p
 
-    if steepp < minp or flatp < minp or steepp > maxp or flatp > maxp:
-        print("Error: Currently must have minp < steepp < flatp < maxp")
-        print("  minp = "), minp
-        print("  steepp = "), steepp
-        print("  flatp = "), flatp
-        print("  maxp = "), maxp
+    if steep_p < min_p or flat_p < min_p or steep_p > max_p or flat_p > max_p:
+        print("Error: Currently must have min_p < steep_p < flat_p < max_p")
+        print("  min_p = "), min_p
+        print("  steep_p = "), steep_p
+        print("  flat_p = "), flat_p
+        print("  max_p = "), max_p
         return None
 
-    s1 = (steepp - minp)/color_width # normalized position of "steep" midpoint
-    f1 = (flatp  - minp)/color_width # normalized position of "flat"  midpoint
+    s1 = (steep_p - min_p)/color_width # normalized position of "steep" midpoint
+    f1 = (flat_p  - min_p)/color_width # normalized position of "flat"  midpoint
     m1 = 0.5*(s1+f1)
 
     # Apply stretches to default
@@ -101,7 +101,7 @@ def create_cmap_specindex(minp,maxp,steepp=-0.8,flatp=-0.1,name="CC-specindex-de
         LCH_x[coord] = np.copy(LCH_x_vals)
 
     # Each parameter horizontally corresponds to LCH_x_vals positions.
-    # Each colour at that position is composed ot the value in L, in C, and in H. So the minp has the colour 85,60,86.
+    # Each colour at that position is composed ot the value in L, in C, and in H. So the min_p has the colour 85,60,86.
     # Luminosity ranges from 0 - 100, Chroma ranges from 0 - 100, Hue is degrees on the colour wheel.
     LCH_y['L'] = [85,    54, 39,     34.3              ,    24,     15.5              ,  15]
     LCH_y['C'] = [60., 74.4,  0,     7.9               ,  25.1,     46.1              ,54.4]
@@ -115,7 +115,42 @@ def create_cmap_specindex(minp,maxp,steepp=-0.8,flatp=-0.1,name="CC-specindex-de
         maps.make_cmap_segmented(LCH_x,LCH_y,name=name,modes=modes,targets=targets,png_dir=png_dir,out=out)
         return name
 
-def create_cmap_specindex_error(c_mid=0.5,L_ends=72,L_mid=50.,L_min=None,L_max=None,C_max=85.,H_0=70.,H_min=None,H_max=None,
+def create_cmap_specindex_constantL(L_0=75,C_0=35,H_start=70.,H_dir='left',name="CC-specindex-constL",modes=['clip','crop'],targets=['mpl','png'],png_dir=".",out=False):
+    """ Makes a new colour map based on Jayanne English's constant Luminosity/chroma colourmap
+        of orange - blue.
+    """
+
+    LCH_x = {}
+    LCH_y = {}
+
+    for coord in ['L','C','H']:
+        LCH_x[coord] = np.arange(0,1.05,0.05)
+
+    # Each parameter horizontally corresponds to LCH_x_vals positions.
+    # Each colour at that position is composed ot the value in L, in C, and in H.
+    # Luminosity ranges from 0 - 100, Chroma ranges from 0 - 100, Hue is degrees on the colour wheel.
+    LCH_y['L'] = [L_0]*len(LCH_x['L'])
+    LCH_y['C'] = [C_0]*len(LCH_x['C'])
+
+    if H_dir == 'left':
+        H_end = H_start - 180.
+    elif H_dir == 'right':
+        H_end = H_start + 180.
+    else:
+        print("Error: H_dir must be 'left' or 'right'")
+        return -1
+
+    LCH_y['H'] = [H_start*(1-i) + H_end*i for i in LCH_x['H']]
+
+    if out:
+        RGB = maps.make_cmap_segmented(LCH_x,LCH_y,name=name,modes=modes,targets=targets,png_dir=png_dir,out=out)
+        return name, RGB
+    else:
+        maps.make_cmap_segmented(LCH_x,LCH_y,name=name,modes=modes,targets=targets,png_dir=png_dir,out=out)
+        return name
+
+
+def create_cmap_specindex_error(c_mid=0.5,L_ends=72,L_mid=50.,L_min=None,L_max=None,C_max=85.,H_0=70.,H_min=None,H_mid=None,H_max=None,
                                 name="CC-specindex-error",modes=['clip','crop'],targets=['mpl','png'],png_dir=".",out=False):
     """ Makes a colour map for uncertainties in spectral index. This is based on Jayanne English's
         error colourmap of light orange and grey, where the pure orange hue indicates the most uncertainty.
@@ -138,12 +173,17 @@ def create_cmap_specindex_error(c_mid=0.5,L_ends=72,L_mid=50.,L_min=None,L_max=N
     if H_min==None and H_max==None:
             H_min = H_0
             H_max = H_0
-    elif H_min==None or H_max==None:
-        print("Error: Currently must set both L_min and L_max, or neither and just set H_0")
+    elif (H_min==None and H_mid==None) or (H_mid==None and H_max==None):
+        print("Error: Currently must set either both of H_min and H_max, or H_mid and one of H_mid and H_max, or none of them and just set H_0")
         return None
 
     C_mid = C_max / 2.
-    H_mid = 0.5*(H_min + H_max)
+    if H_mid==None:
+        H_mid = 0.5*(H_min + H_max)
+    elif H_min==None:
+        H_min=H_mid
+    elif H_max==None:
+        H_max=H_mid
 
     LCH_x_vals = [ 0,  c_mid, 1]
     LCH_x = {}
@@ -168,16 +208,16 @@ def create_cmap_specindex_error(c_mid=0.5,L_ends=72,L_mid=50.,L_min=None,L_max=N
         return name
 
 
-def create_cmap_velocity(minp,maxp,div=0.0, width=0.0):
+def create_cmap_velocity(min_p,max_p,div=0.0, width=0.0):
     """ Makes a color map based on Jayanne English's velocity colourmap
         of yellow - plum, where the orange and dark cyan points
         are fixed to the steep and flat points, while the outer
         regions extend to the min and max values provided.
     """
     # Default:
-    color_width = maxp - minp
+    color_width = max_p - min_p
 
-    d0 = float(div - minp)/float(color_width) # normalized position of midpoint
+    d0 = float(div - min_p)/float(color_width) # normalized position of midpoint
 
     points = {}
     values = {}
