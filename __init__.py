75cb19764e02197b80f7176cd56154ddcdbacc22
Mark Richardson <Mark.Richardson.Work@gmail.com>
2023-08-14 18:37:04 -0400

    Create __init__.py
    Adding __init__ file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Mark Richardson, Jayanne English, Nathan Deg"
+__version__ = "1.0"
+
+from . import galfits as gal
+from . import specindex as spx
+from . import velmap as vmap
